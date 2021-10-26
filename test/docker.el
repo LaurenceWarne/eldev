0b83c0cc52d6114333e95e3ee2c81a0401790490
lWarne <laurencewarne@gmail.com>
2021-10-26 11:18:17 +0100

    Fix tests

--- test/docker.el
+++ test/docker.el
@@ -1,24 +1,16 @@
 (require 'test/common)
 
-(defvar eldev--emacs-docker-version "27.2")
-
 (ert-deftest eldev-emacs-docker-emacs-1 ()
-  (skip-unless (executable-find "docker"))
-  (eldev--test-run "trivial-project"
-      ("docker" eldev--emacs-docker-version "emacs" "--batch")
-    (should (= exit-code 0))))
-
-(ert-deftest eldev-emacs-docker-emacs-2 ()
-  (skip-unless (executable-find "docker"))
+  (skip-unless (eldev-docker-executable nil))
   (eldev--test-run "trivial-project"
       ("--quiet"
        "docker"
-       eldev--emacs-docker-version
+       "27.2"
        "emacs"
        "--batch"
        "--eval"
        `(prin1 (+ 1 2)))
-    (should (string= (substring (string-trim-right stdout "\n*") -1) "3"))
+    (should (string= (substring (string-trim-right stdout) -1) "3"))
     (should (= exit-code 0))))
 
 (provide 'test/emacs-docker)
